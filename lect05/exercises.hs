daf7cfe3a5564ef38ca22be0cd56a5d44114019a
stefano <svozza@gmail.com>
2015-12-12 12:33:56 +0000

    add exercises for lecture 5

---
+++ lect05/exercises.hs
@@ -0,0 +1,13 @@
+-- Ex01
+pyths :: Int -> [(Int, Int, Int)]
+pyths n = [(x, y, z) | x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
+
+--Ex02
+factors  :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1..n], sum (init (factors x)) == x]
+
+scalar :: [Int] -> [Int] -> [Int]
+scalar xs ys = [(\(x, y) -> x * y) zipped | zipped <- zip xs ys]
