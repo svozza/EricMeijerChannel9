946a9348e43adefe92112d86cad48f13203ff8c3
stefano <svozza@gmail.com>
2015-12-14 16:56:12 +0000

    add rest of exercise 1

--- lect06/exercises.hs
+++ lect06/exercises.hs
@@ -1,3 +1,24 @@
+-- Ex01 (a)
+and' :: [Bool] -> Bool
+and' [] = True
+and' (x:xs) = x && and' xs
+
+-- Ex01 (b)
 concat' :: [[a]] -> [a]
 concat' [] = []
 concat' (xs:xss) = xs ++ concat' xss
+
+-- Ex01 (c)
+replicate' :: Int -> a -> [a]
+replicate' 0 _ = []
+replicate' n x = [x] ++ replicate' (n - 1) x
+
+-- Ex01 (d)
+nth :: [a] -> Int -> a
+nth (x:_) 0 = x
+nth (_:xs) n = nth xs (n - 1)
+
+-- Ex01 (e)
+elem' :: Eq a => a -> [a] -> Bool
+elem' _ [] = False
+elem' e (x:xs) = e == x || elem' e xs
