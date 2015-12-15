bf3e8f5398e01c0bfcb90994ad4d2e0fa04c3c54
stefano <svozza@gmail.com>
2015-12-15 10:59:25 +0000

    add final exercises

--- lect06/exercises.hs
+++ lect06/exercises.hs
@@ -22,3 +22,16 @@ nth (_:xs) n = nth xs (n - 1)
 elem' :: Eq a => a -> [a] -> Bool
 elem' _ [] = False
 elem' e (x:xs) = e == x || elem' e xs
+
+-- Ex02
+merge :: [Int] -> [Int] -> [Int]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) | x < y = x:(merge xs (y:ys))
+                    | otherwise = y:(merge (x:xs) ys)
+
+-- Ex03
+msort :: [Int] -> [Int]
+msort xs | length xs <= 1 = xs
+         | otherwise = merge (msort h1) (msort h2)
+           where (h1, h2) = splitAt (div (length xs) 2) xs
